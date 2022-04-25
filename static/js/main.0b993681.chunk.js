4acde30f87d60143972aa9764a49e29763c3d098
Code Sandbox <hello@codesandbox.io>
2022-04-25 10:42:15 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.0b993681.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact=this.webpackJsonpreact||[]).push([[0],{12:function(e,t,c){},14:function(e,t,c){"use strict";c.r(t);var r=c(1),n=c(3),s=(c(12),c(0));function o(){return Object(s.jsxs)("div",{className:"App",children:[Object(s.jsx)("h1",{children:"Hello Mr. Troy Sambatoos"}),Object(s.jsx)("h2",{children:"StarT editing to see some magic happen!"})]})}var i=document.getElementById("root");Object(n.createRoot)(i).render(Object(s.jsx)(r.StrictMode,{children:Object(s.jsx)(o,{})}))}},[[14,1,2]]]);
+//# sourceMappingURL=main.0b993681.chunk.js.map
